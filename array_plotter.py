aa0e373a487aee529af87b5878205f59ec59936e
Husnain Raza <hhrraazzaa@gmail.com>
2020-08-23 18:44:26 -0500

    More sorting algorithms

--- array_plotter.py
+++ array_plotter.py
@@ -12,8 +12,9 @@ matplotlib.use('TkAgg')
 SWAP_COLOR = "yellow"
 ACCESS_COLOR = "green"
 COMPARE_COLOR = "red"
-INSERT_COLOR = "purple"
-DEFAULT_COLOR = "blue"
+INSERT_COLOR = "blue"
+DEFAULT_COLOR = "white"
+FINISH_COLOR = "lime"
 
 class Array:
     "Class that stores an array as well as implementing special methods and keeping track of various statistics"
@@ -25,7 +26,7 @@ class Array:
         self.accesses = 0
         self.comparisons = 0
         self.swaps = 0
-        self.labels = labels
+        self.labels = labels # labels will be an array with same length as given array where labels[i] is the color of array[i], else None
         self.verbose = verbose
         self.start_time = time()
         self.history = []
@@ -101,11 +102,18 @@ class Array:
         out["accesses"] = self.accesses
         out["comparisons"] = self.comparisons
         out["swaps"] = self.swaps
-        out["labels"] = copy(self.labels)
+        out["labels"] = copy(self.labels) 
         out["action"] = action
         out["permutation"] = copy(self.permutation)
         out["time"] = time() - self.start_time
         return out
+
+    def finish(self):
+        labels = [None for i in range(len(self.array))]
+        for i in range(len(labels)):
+            labels[i] = FINISH_COLOR
+            self.labels = labels
+            self.history.append(self.summary())
 
     def update_permutation(self):
         self.permutation = [i.index for i in self.array]
@@ -159,16 +167,16 @@ def plot_frame(frame, filename):
         index1, index2 = frame["action"]["args"]
         if index1: bars_colors[index1] = COMPARE_COLOR
         if index2: bars_colors[index2] = COMPARE_COLOR
-    ax.text(0.5,1.01, summary_string, size=10, color="black", transform = ax.transAxes)
+    ax.text(0.5,1.01, summary_string, size=14, color="black", transform = ax.transAxes)
     ax.bar(range(len(bars)), bars, color = bars_colors, edgecolor = "black", linewidth=1)
     ax.set_xticks([])
     ax.set_yticks([])
     plt.savefig(filename)
     plt.close("all")
 
-def plot_history_bar(frames):
+def plot_history_bar(frames, name = ""):
     plt.style.use("dark_background")
-    fig, ax = plt.subplots(figsize=(10,4))
+    fig, ax = plt.subplots(figsize=(10,10))
     def animate(i):
         print(i)
         frame = frames[i]
@@ -178,6 +186,10 @@ def plot_history_bar(frames):
             "swaps: " + str(frame["swaps"]) + "\n" + \
             "elapsed time (s): " + str(frame["time"])
         bars_colors = [DEFAULT_COLOR for i in range(len(bars))]
+        if frame["labels"]:
+            for i in range(len(frame["labels"])):
+                if frame["labels"][i]:
+                    bars_colors[i] = frame["labels"][i]
         if frame["action"]["type"] == "access":
             key = frame["action"]["args"]
             bars_colors[key] = ACCESS_COLOR
@@ -194,6 +206,7 @@ def plot_history_bar(frames):
             if index2: bars_colors[index2] = COMPARE_COLOR
         ax.clear()
         ax.text(0.5,1.01, summary_string, size=8, color="white", transform = ax.transAxes)
+        ax.text(0.5,-0.1, name, size=28, color="white", transform = ax.transAxes)
         ax.bar(range(len(bars)), bars, color = bars_colors, edgecolor = "black", linewidth=1)
         ax.set_xticks([])
         ax.set_yticks([])
@@ -226,15 +239,16 @@ def plot_history_line(frames):
 # Various sorting algorithms
 
 
-def selection_sort(arr):
+def selection_sort(arr, finish=False):
     for i in range(len(arr)):
         min_id = i
         for j in range(i+1, len(arr)):
             if arr[min_id] > arr[j]:
                 min_id = j
         arr.swap(i, min_id)
+    if finish: arr.finish()
 
-def insertion_sort_efficient(arr):
+def insertion_sort(arr, finish=False):
     for i in range(1, len(arr)):
         key = arr[i]
         j = i-1
@@ -242,11 +256,104 @@ def insertion_sort_efficient(arr):
             arr[j+1] = arr[j]
             j -= 1
         arr[j+1] = key
+    if finish: arr.finish()
 
+def bubble_sort(arr, finish=False):
+    for i in range(len(arr)):
+        for j in range(len(arr)-i-1):
+            if arr[j] > arr[j+1]:
+                arr.swap(j,j+1)
+    if finish: arr.finish()
 
-arr = Array([i+1 for i in reversed(range(20))],verbose=False)
+def slow_sort(arr, finish=False):
+    def slow_sort_(arr, i, j):
+        if i >= j: return
+        m = int(0.5 * (i+j))
+        slow_sort_(arr, i, m)
+        slow_sort_(arr, m+1, j)
+        if arr[j] < arr[m]:
+            arr.swap(j,m)
+        slow_sort_(arr,i,j-1)
+    slow_sort_(arr, 0, len(arr)-1)
+    if finish: arr.finish()
+
+def stooge_sort(arr, finish=False):
+    def stooge_sort_(arr, i, j):
+        if arr[i] > arr[j]:
+            arr.swap(i,j)
+        if (j-i+1) > 2:
+            t = (j-i+1)/3
+            stooge_sort_(arr,i,j-t)
+            stooge_sort_(arr,i+t,j)
+            stooge_sort_(arr,i,j-t)
+        return arr
+    stooge_sort_(arr, 0, len(arr)-1)
+    if finish: arr.finish()
+
+def quick_sort(arr, finish=False, label=True):
+    LO_COLOR = "#ff00aa"
+    HI_COLOR = "#00ffa0"
+    PART_COLOR = "#00ffff"
+    labels = None
+    if label: 
+        arr.labels = [None for i in range(len(arr))]
+    def quick_sort_(arr,lo,hi):
+        if label and lo < hi: 
+            arr.labels = [None for i in range(len(arr))]
+            arr.labels[lo] = LO_COLOR
+            arr.labels[hi] = HI_COLOR
+        if lo < hi:
+            p = partition_(arr,lo,hi)
+            if arr.labels: arr.labels[p] = PART_COLOR
+            quick_sort_(arr,lo,p-1)
+            quick_sort_(arr,p+1,hi)
+    def partition_(arr,lo,hi):
+        pivot = arr[hi]
+        i = lo
+        for j in range(lo,hi+1):
+            if arr[j] < pivot:
+                arr.swap(i,j)
+                i += 1
+        arr.swap(i,hi)
+        return i
+    quick_sort_(arr,0,len(arr)-1)
+    if finish: arr.finish()
+
+def shell_sort(arr, finish=False):
+    gaps = [701, 301, 132, 57, 23, 10, 4, 1]
+    for gap in gaps:
+        for i in range(gap,len(arr)):
+            temp = arr[i]
+            j = i
+            while j >= gap and arr[j-gap] > temp:
+                arr[j] = arr[j-gap]
+                j -= gap
+            arr[j] = temp
+    if finish: arr.finish()
+
+def cocktail_sort(arr, finish=False):
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(arr)-1):
+            if arr[i] > arr[i+1]:
+                arr.swap(i,i+1)
+                swapped = True
+        if not swapped:
+            break
+        swapped = False
+        for i in reversed(range(len(arr)-1)):
+            if arr[i] > arr[i+1]:
+                arr.swap(i,i+1)
+                swapped = True
+    if finish: arr.finish()
+
+def odd_even_sort():
+    pass
+
+arr = Array([i+1 for i in reversed(range(10))],verbose=False)
 print(arr)
-selection_sort(arr)
+cocktail_sort(arr, True)
 print(len(arr.history))
-test = plot_history_bar(arr.history)
-test.save("selection.avi")
+test = plot_history_bar(arr.history,name="Shell Sort")
+test.save("out.mp4")
